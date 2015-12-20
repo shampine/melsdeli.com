02c1a26517693a39d23ffddb693d48fa4b5c53f4
patrickshampine <patrick@patrickshampine.com>
2015-12-19 17:12:08 -0800

    Added delivery

--- public/assets/themes/wordplate/footer.php
+++ public/assets/themes/wordplate/footer.php
@@ -3,10 +3,11 @@
   $front = get_option('page_on_front');
 
   $meta = (object) array(
-    "hours"   => get_field('hours', $front),
-    "address" => get_field('address', $front),
-    "phone"   => get_field('phone', $front),
-    "map"     => get_field('map', $front),
+    "hours"    => get_field('hours', $front),
+    "delivery" => get_field('delivery', $front),
+    "address"  => get_field('address', $front),
+    "phone"    => get_field('phone', $front),
+    "map"      => get_field('map', $front),
   ); ?>
 
   <hr>
@@ -14,13 +15,13 @@
     <div class="row">
       <div class="col-sm-10 col-sm-offset-1"><?php
         if($meta->hours) {
+          echo '<p class="bold">Hours</p>';
           echo '<p>'.$meta->hours.'</p>';
+        }
+        if($meta->delivery) {
+          echo '<p class="bold">Delivery</p>';
+          echo '<p>'.$meta->delivery.'</p>';
         } ?>
-        <ul class="social">
-          <li><a href="https://www.facebook.com/MelsDeliDTLA/" target="_blank"><i class="fa fa-facebook"></i></a></li>
-          <li><a href="https://www.instagram.com/melsdelidtla/" target="_blank"><i class="fa fa-instagram"></i></a></li>
-          <li><a href="http://www.yelp.com/biz/mels-deli-los-angeles" target="_blank"><i class="fa fa-yelp"></i></a></li>
-        </ul>
         <div class="location"><?php
           if($meta->address !== false) {
             if($meta->map !== false) {
@@ -33,6 +34,11 @@
             echo '<p>'.$meta->phone.'</p>';
           } ?>
         </div>
+        <ul class="social">
+          <li><a href="https://www.facebook.com/MelsDeliDTLA/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="https://www.instagram.com/melsdelidtla/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+          <li><a href="http://www.yelp.com/biz/mels-deli-los-angeles" target="_blank"><i class="fa fa-yelp"></i></a></li>
+        </ul>
       </div>
     </div>
   </div>
